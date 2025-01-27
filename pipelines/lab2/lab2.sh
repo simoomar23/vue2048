337088767dea6a847f17e06e27756c6f1f537408
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2025-01-27 21:33:33 +0100

    Add lab2 correction

---
+++ pipelines/lab2/lab2.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+
+## Partie Pour aller plus loin ##
+echo "Identification des dépendances obsolètes"
+pnpm outdated --format json > reports/outdated-dependencies.json
+# pnpm up --latest
+
+echo "Identification des dépendances vulnérables"
+pnpm audit --json  > reports/vulnerable-dependencies.json   
+# pnpm audit --fix
+## Partie Pour aller plus loin ##
+
+echo "Installation des dépendances du projet"
+pnpm install
+
+echo "Type checking" 
+pnpm vue-tsc --noEmit
+
+echo "Installation des dépendances du projet"
+pnpm vite build
+
+echo "Analyse statique"
+pnpm eslint .
+
+# echo "Lancement du package créé" 
+# pnpm vite preview
+
+echo "Execution des tests"
+pnpm vitest run
